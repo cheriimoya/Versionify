9bb24adb17abbf171d7d02fc4ff0bb213e5f3fba
Max Hausch <max.hausch@helsinki-systems.de>
2022-07-02 17:52:17 +0200

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> {}, src ? ./. }:
+pkgs.rustPlatform.buildRustPackage {
+  name = "versionify";
+  inherit src;
+  nativeBuildInputs = with pkgs; [
+    pkgconfig
+    openssl
+  ];
+
+  PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+
+  cargoLock.lockFile = ./Cargo.lock;
+  meta = with pkgs.lib; {
+    description = "Playlist versioning to git for Spotify";
+    homepage = "https://github.com/cheriimoya/versionify";
+    license = licenses.mit;
+    maintainers = with maintainers; [ cheriimoya ];
+    platforms = platforms.unix;
+  };
+}
